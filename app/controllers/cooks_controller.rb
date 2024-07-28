a80feab0b80392d688989aace4e7594178e432a1
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2024-07-28 19:21:48 +0300

    update cooks controller

--- app/controllers/cooks_controller.rb
+++ app/controllers/cooks_controller.rb
@@ -1,12 +1,17 @@
 class CooksController < ApplicationController
+  before_action :set_cook, only: [:show, :update, :destroy]
+
   def index
-    @cooks = Cook.all
+    @cooks = Cook.where(is_deleted: false)
     render json: @cooks
   end
 
   def show
-    @cook = Cook.find(params[:id])
-    render json: @cook
+    if @cook.is_deleted
+      render json: { message: "Cook not found" }, status: 404
+    else
+      render json: @cook
+    end
   end
 
   def create
@@ -19,8 +24,7 @@ class CooksController < ApplicationController
   end
 
   def update
-    @cook = Cook.find(params[:id])
-    if @cook.update(cook_params)
+    if @cook.update(cook_params) && !@cook.is_deleted
       render json: @cook
     else
       render json: { errors: @cook.errors.full_messages }, status: 422
@@ -28,7 +32,6 @@ class CooksController < ApplicationController
   end
 
   def destroy
-    @cook = Cook.find(params[:id])
     # @cook.destroy
     @cook.update(is_deleted: true, deleted_at: Time.now, password: params[:password])
     if @cook.save
@@ -51,5 +54,9 @@ class CooksController < ApplicationController
 
   def cook_params
     params.require(:cook).permit(:name, :email, :password, :is_deleted)
+  end
+
+  def set_cook
+    @cook = Cook.find(params[:id])
   end
 end
