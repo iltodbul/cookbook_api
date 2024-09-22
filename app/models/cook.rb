5a0838c219fd5b64710c06ff8224c6a6f38330f6
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2024-09-22 21:05:16 +0300

    add relations

--- app/models/cook.rb
+++ app/models/cook.rb
@@ -14,15 +14,19 @@
 #
 class Cook < ApplicationRecord
   has_secure_password
+  before_save { self.email = email.downcase }
+  before_save { self.is_deleted = false }
+
   # has_many :recipes
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
   validates :password, presence: true, length: { minimum: 6 }
 
   def self.authenticate(email, password)
-    cook = Cook.find_by(email: email)
+    cook = Cook.find_by(email: email.downcase)
     cook && cook.authenticate(password)
   end
 end
 
 #Cook.create(name: "Cook3", is_deleted: false, email: "adc@test.it", password: "hashPass3")
+#Cook.create(name: "Ilo", email: "a@a.com", password: "1234567")
