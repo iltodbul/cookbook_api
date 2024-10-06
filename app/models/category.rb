2b7368ddab2ab4fb6bc55e560a1f6adba7f0c465
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2024-10-06 13:02:48 +0300

    update category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -10,4 +10,17 @@
 #  name       :text
 #
 class Category < ApplicationRecord
+  # Relationships
+  has_many :recipes
+
+  # Validations
+  validates :name, presence: true
+  validates :name, length: { minimum: 3 }
+  validates :name, length: { maximum: 30 }
+  validates :name, format: { with: /\A[a-zA-Z\s]+\z/, message: "only allows letters" }
+  validates :name, format: { without: /\d/, message: "does not allow numbers" }
+  validates :name, format: { without: /\W/, message: "does not allow special characters" }
+
+  # Callbacks
+  before_save { self.is_deleted = false }
 end
